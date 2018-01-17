eb4d3c4c02af24470ebf80aa5f30968c39ce19b5
Georg <georg.goetz@tu-ilmenau.de>
2018-01-17 14:31:51 +0100

    First Version of Highscore System
    Implemented first version of highscore system. Every Minigame can now implement a ScoreManager which saves the highscores to playerprefs and displays them in the main menu.

---
+++ Assets/Scripts/GUI/AddUpScores.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddUpScores : MonoBehaviour {
+    
+   	void Update () 
+    {
+        Text totalScoretext = GetComponent<Text>();
+
+        GameObject[] scores = GameObject.FindGameObjectsWithTag("Score");
+
+        int totalscore = 0;
+
+        foreach (GameObject score in scores)
+        {
+            Text scoreText = score.GetComponent<Text>();
+            totalscore += int.Parse(scoreText.text);
+        }
+
+        totalScoretext.text = totalscore.ToString();
+	}
+}
