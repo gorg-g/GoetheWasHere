2f9f8916ddbdbc615a69154a3997696b676f65da
Micha <michael.ebneth@tu-ilmenau.de>
2018-01-28 20:08:43 +0100

    CR - 1

---
+++ Assets/Scripts/MinigameXX-CircuitRacer/Checkpoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+
+    public GameObject boden;
+
+    private bool OnCollisionEnter(Collision collision)
+    {
+        return true;
+
+    }
+}
---
+++ Assets/Scripts/MinigameXX-CircuitRacer/ElectronController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectronController : MonoBehaviour {
+
+    public float speed;
+
+    public VirtualJoystick joystick;
+
+    private Rigidbody rb;
+
+
+    void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float x = joystick.Horizontal();
+        float y = joystick.Vertical();
+
+        Vector3 movement = new Vector3(x, 0, y);
+
+        rb.velocity = movement * speed;
+    }
+}
